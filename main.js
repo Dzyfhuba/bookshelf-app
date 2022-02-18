24eeaf20175666f9916f5967afb68f9c65cbfbc1
Dzyfhuba <hafidz21ub@gmail.com>
2022-02-19 01:15:41 +0700

    add complete mark toggle

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ if (typeof(Storage) !== 'undefined') {
 
     displayBook(book);
 
-    document.getElementById('searchBook').addEventListener('submit', function(e) {
-        e.preventDefault();
-        compareBook = book.filter(function(item) {
-            return item.title.toLowerCase().includes(document.getElementById('searchBookTitle').value.toLowerCase());
-        });
-        document.getElementById('incompleteBookshelfList').innerHTML = '';
-        displayBook(compareBook);
+    document.getElementById('searchBook').addEventListener('submit', searchBook());
+
+    document.querySelectorAll('#markAsCompleted, #markAsNotCompleted').forEach(e => {
+        e.addEventListener('click', toggleCompleteMark());
     });
 
     inputBook = document.getElementById('inputBook');
@@ -22,20 +19,49 @@ if (typeof(Storage) !== 'undefined') {
     alert("Browser yang Anda gunakan tidak mendukung Web Storage")
 }
 
+function toggleCompleteMark() {
+    return function(e) {
+        e.preventDefault();
+        book.forEach(b => {
+            if (b.id == e.target.parentElement.parentElement.getAttribute('book-id')) {
+                b.isComplete = !b.isComplete;
+                console.log(b.isComplete);
+            }
+        });
+        localStorage.setItem('book', JSON.stringify(book));
+        document.getElementById('incompleteBookshelfList').innerHTML = '';
+        document.getElementById('completeBookshelfList').innerHTML = '';
+        window.location.reload();
+        displayBook(book);
+    };
+}
+
+function searchBook() {
+    return function(e) {
+        e.preventDefault();
+        compareBook = book.filter(function(item) {
+            return item.title.toLowerCase().includes(document.getElementById('searchBookTitle').value.toLowerCase());
+        });
+        document.getElementById('incompleteBookshelfList').innerHTML = '';
+        document.getElementById('completeBookshelfList').innerHTML = '';
+        displayBook(compareBook);
+    };
+}
+
 function displayBook(book) {
     notComplete = book.filter(function(item) {
         return item.isComplete == false;
     });
     notComplete.forEach(e => {
         document.getElementById('incompleteBookshelfList').innerHTML += `
-        <article class="book_item">
+        <article class="book_item" book-id="${e.id}">
             <h3>${e.title}</h3>
             <p>Penulis: ${e.author}</p>
             <p>Tahun: ${e.year}</p>
 
             <div class="action">
-                <button class="green">Selesai dibaca</button>
-                <button class="red">Hapus buku</button>
+                <button id="markAsCompleted" class="green">Selesai Dibaca</button>
+                <button id="deleteBook" class="red">Hapus Buku</button>
             </div>
         </article>
         `;
@@ -45,14 +71,14 @@ function displayBook(book) {
     });
     hasComplete.forEach(e => {
         document.getElementById('completeBookshelfList').innerHTML += `
-        <article class="book_item">
+        <article class="book_item" book-id="${e.id}">
             <h3>${e.title}</h3>
             <p>Penulis: ${e.author}</p>
             <p>Tahun: ${e.year}</p>
 
             <div class="action">
-                <button class="green">Selesai dibaca</button>
-                <button class="red">Hapus buku</button>
+                <button id="markAsNotCompleted" class="green">Belum Selesai Dibaca</button>
+                <button id="deleteBook" class="red">Hapus Buku</button>
             </div>
         </article>
         `;
