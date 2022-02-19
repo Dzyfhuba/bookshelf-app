db03f394f6068cfc1673dbc28df34d938a39e8ac
Dzyfhuba <hafidz21ub@gmail.com>
2022-02-19 17:22:41 +0700

    add search, delete, etc
    next: styling

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bookself Apps
+
+you can check it yourself [here](https://dzyfhuba.github.io/bookshelf-app)
--- main.js
+++ main.js
@@ -9,17 +9,75 @@ if (typeof(Storage) !== 'undefined') {
 
     document.getElementById('searchBook').addEventListener('submit', searchBook());
 
+    clear = document.getElementById('clear');
+    clear.addEventListener('click', function(e) {
+        e.previousElementSibling.value = '';
+        e.focus();
+    });
+
     document.querySelectorAll('#markAsCompleted, #markAsNotCompleted').forEach(e => {
-        e.addEventListener('click', toggleCompleteMark());
+        e.addEventListener('click', toggleCompleteMark(e));
+    });
+
+    document.querySelectorAll('#deleteBook').forEach(e => {
+        e.addEventListener('click', deleteBook(e));
     });
 
     inputBook = document.getElementById('inputBook');
     inputBook.addEventListener('submit', insertBook());
+
+    var btn = document.querySelectorAll("#deleteBookModal");
+    btn.forEach(e => {
+        e.addEventListener('click', openDeleteBookModal(e));
+    });
+    closeModal = document.querySelectorAll(".close");
+    closeModal.forEach(e => {
+        e.addEventListener('click', closeDeleteBookModal(e));
+    });
+    window.addEventListener('click', closeDeleteBookModal2(e));
 } else {
     alert("Browser yang Anda gunakan tidak mendukung Web Storage")
 }
 
-function toggleCompleteMark() {
+function closeDeleteBookModal2(e) {
+    return function(e) {
+        if (e.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+}
+
+function closeDeleteBookModal(e) {
+    return function() {
+        modal = e.parentElement.parentElement.parentElement;
+        modal.style.display = "none";
+    };
+}
+
+function openDeleteBookModal(e) {
+    return function() {
+        modal = e.nextElementSibling;
+        modal.style.display = "block";
+    };
+}
+
+function deleteBook(e) {
+    return function(e) {
+        e.preventDefault();
+        book.forEach(b => {
+            if (b.id == e.target.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute('book-id')) {
+                book.splice(book.indexOf(b), 1);
+            }
+        });
+        localStorage.setItem('book', JSON.stringify(book));
+        document.getElementById('incompleteBookshelfList').innerHTML = '';
+        document.getElementById('completeBookshelfList').innerHTML = '';
+        window.location.reload();
+        displayBook(book);
+    };
+}
+
+function toggleCompleteMark(e) {
     return function(e) {
         e.preventDefault();
         book.forEach(b => {
@@ -61,7 +119,17 @@ function displayBook(book) {
 
             <div class="action">
                 <button id="markAsCompleted" class="green">Selesai Dibaca</button>
-                <button id="deleteBook" class="red">Hapus Buku</button>
+                <button id="deleteBookModal" class="red">Hapus</button>
+                <div id="myModal" class="modal">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <span class="close">&times;</span>
+                        </div>
+                        <div class="modal-body">
+                            <button id="deleteBook" class="red">Hapus Buku</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </article>
         `;
@@ -78,7 +146,17 @@ function displayBook(book) {
 
             <div class="action">
                 <button id="markAsNotCompleted" class="green">Belum Selesai Dibaca</button>
-                <button id="deleteBook" class="red">Hapus Buku</button>
+                <button id="deleteBookModal" class="red">Hapus</button>
+                <div id="myModal" class="modal">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <span class="close">&times;</span>
+                        </div>
+                        <div class="modal-body">
+                            <button id="deleteBook" class="red">Hapus Buku</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </article>
         `;
